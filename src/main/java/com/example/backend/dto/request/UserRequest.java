caa1f0fc6ea3f30b52be62db9b7c578c2862ead9
benissamoncef <moncef.benissa@etu.uae.ac.ma>
2024-02-02 10:31:57 +0100

    first commit

---
+++ src/main/java/com/example/backend/dto/request/UserRequest.java
@@ -0,0 +1,42 @@
+package com.example.backend.dto.request;
+import java.io.Serializable;
+import java.util.Date;
+
+public class UserRequest implements Serializable {
+    private String name;
+    private String gender;
+    private Date dateOfBirth;
+    private String Address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
